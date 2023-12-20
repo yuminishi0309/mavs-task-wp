4e807cac6b125f7d60851707a49e52859c30b39b
YumiN <yuminishi0309@gmail.com>
2023-12-20 19:21:43 +0900

    add: top-page css layout

--- themes/mavs/assets/js/script.js
+++ themes/mavs/assets/js/script.js
@@ -1,2 +1,19 @@
 const num = 27
 console.log(num)
+
+$(function () {
+    // ハンバーガーメニューボタン
+    $('#btn').on('click', function(){
+        $('#btn__top').toggleClass('rotate-top');
+        $('#btn__middle').toggleClass('hide-middle');
+        $('#btn__bottom').toggleClass('rotate-bottom');
+        $('#gnavFadeIn').toggleClass('gnav-active');
+    });
+    // ナビゲーション操作
+    $('.gnavFadeIn__link').on('click', function () {
+        $('#btn__top').removeClass('rotate-top');
+        $('#btn__middle').removeClass('hide-middle');
+        $('#btn__bottom').removeClass('rotate-bottom');
+        $('#gnavFadeIn').removeClass('gnav-active');
+    })
+});
