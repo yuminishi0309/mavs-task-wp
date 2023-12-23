876dd247936a3aa2c0edb1c66547a2c50ade602b
YumiN <yuminishi0309@gmail.com>
2023-12-23 12:37:32 +0900

    修正: READ MOREボタン, swiper, mvアニメーション微調整(仮), ほか微調整

--- themes/mavs/assets/js/swiper.js
+++ themes/mavs/assets/js/swiper.js
@@ -1,30 +1,41 @@
 const swiperBlog = new Swiper('.swiper-blog', {
-        direction: 'horizontal',
-        loop: true,
-        effect: 'slide',
-        slidesPerView: 3,
-        spaceBetween: 20,
-        speed: 4000,
-        autoplay : {
-            delay: 1,
-            disableOnInteraction: true,
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    // autoplay: true,
+    autoplay : {
+    delay: 2000,
+    disableOnInteraction: false,
+    },
+    // レスポンシブ設定
+    breakpoints:{
+        // Tab (when window width is >= 768px)
+        768:{
+            slidesPerView: 3,
+        },
+        // PC (when window width is >= 1024px)
+        1024:{
+            slidesPerView: 4,
+            spaceBetween: 26,
         }
+    }
 });
 // swiperレスポンシブ対応--------------------------------------------
-function updateSwiper() {
-    const screenWidth = window.innerWidth;
-    if (screenWidth >= 1024) {
-        swiperBlog.params.slidesPerView = 4;
-    } else if (screenWidth >= 768) {
-        swiperBlog.params.slidesPerView = 3;
-    } else {
-        swiperBlog.params.slidesPerView = 1;
-    }
-    swiperBlog.update();
-}
-// 初回の設定
-updateSwiper();
-// 画面サイズ変更時の処理
-window.addEventListener('resize', function() {
-    updateSwiper();
-});
+// function updateSwiper() {
+//     const screenWidth = window.innerWidth;
+//     if (screenWidth >= 1024) {
+//         swiperBlog.params.slidesPerView = 4;
+//     } else if (screenWidth >= 768) {
+//         swiperBlog.params.slidesPerView = 3;
+//     } else {
+//         swiperBlog.params.slidesPerView = 1.5;
+//     }
+//     swiperBlog.update();
+// }
+// // 初回の設定
+// updateSwiper();
+// // 画面サイズ変更時の処理
+// window.addEventListener('resize', function() {
+//     updateSwiper();
+// });
