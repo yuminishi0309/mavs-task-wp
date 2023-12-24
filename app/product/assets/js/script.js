6af2fce1f5375a30f8fd5013daf2f3f05a799803
YumiN <yuminishi0309@gmail.com>
2023-12-24 17:37:42 +0900

    追加：blog/blog-singleページのcssレイアウト

--- app/product/assets/js/script.js
+++ app/product/assets/js/script.js
@@ -1,7 +1,7 @@
-const num = 27
-console.log(num)
-
 $(function () {
+    /* ================================================================
+    top page
+    ================================================================ */
     // ハンバーガーメニューボタン
     $('#btn').on('click', function(){
         $('#btn__top').toggleClass('rotate-top');
@@ -16,4 +16,27 @@ $(function () {
         $('#btn__bottom').removeClass('rotate-bottom');
         $('#gnavFadeIn').removeClass('gnav-active');
     })
+
+    /* ================================================================
+    blog page
+    ================================================================ */
+    // トグルメニュー  =======================================
+    $('#blogListMenu').on('click', function(){
+        $('#blogListNav').stop().slideToggle(300);
+        $('#blogListMenu').toggleClass('RotateItem');
+    });
+    // nav操作時処理
+    $('#blogListNav').on('click', function(){
+        $('#blogListNav').hide(300);
+    });
+    // ナビゲーションカレント表示  =======================================
+    const links = document.querySelectorAll(".blogListNav__link");
+    links.forEach(function (link) {
+        if (link.href === location.href) {
+            link.closest(".blogListNav__link").classList.add("current");
+        }
+    });
+    /* ================================================================
+    contact page
+    ================================================================ */
 });
